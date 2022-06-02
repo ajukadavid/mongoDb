2830ce354212b4585dd01a6e629a6c7c48e9bf83
Ajuka David <ajuka.zephiniah@gmail.com>
2022-06-02 21:45:35 +0100

    mongo db database now connecting

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb:localhost/students_test", {useNewUrlParser: true})
+mongoose.connect("mongodb://localhost/students_test", {useNewUrlParser: true})
 
 mongoose.connection
     .once("open", () => console.log("We are connected"))
